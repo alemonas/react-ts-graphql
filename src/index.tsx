6d578d4858e2a3a76381c61a3ffde2757c0e15c6
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2020-04-23 22:53:11 -0400

    aliases react-redux as ReduxProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ReduxProvider store={store}>
       <App />
-    </Provider>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
