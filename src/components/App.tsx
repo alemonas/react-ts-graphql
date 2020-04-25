e9c38a34789b98d31879f4da83ed481cbe4967bf
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2020-04-24 21:27:28 -0400

    wip add routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import theme from '../theme';
+import theme from "../theme";
+import Home from "../routes/home/Home";
+import About from "../routes/about/About";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />      
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about-us">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ src/routes/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      About Us Page
+    </div>
+  );
+}
+
+export default About;
