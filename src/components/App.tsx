0d245a7c86c92d4e5df747f52f75cb940349d2d8
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2020-05-03 17:53:13 -0400

    add pagenotFound and posts example in home

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,39 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 import { ThemeProvider } from "@material-ui/core/styles";
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import React, { lazy, Suspense } from "react";
+import { Router, Route, Switch } from "react-router-dom";
 
 import theme from "../theme";
-import Home from "../routes/home/Home";
-import About from "../routes/about/About";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
-function App() {
-  return (
+const Home = lazy(() => import("../routes/home/Home"));
+const About = lazy(() => import("../routes/about/About"));
+
+const App: React.FC = () => (
+  <Router history={createBrowserHistory()}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about-us">
-            <About />
-          </Route>
-        </Switch>
-      </Router>
+      <Header />
+      <Container>
+        <Suspense fallback={"loading..."}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about-us">
+              <About />
+            </Route>
+            <Route>
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Container>
     </ThemeProvider>
-  );
-}
+  </Router>
+);
 
 export default App;
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Typography, Grid, Card, CardContent } from '@material-ui/core';
 
 function Home() {
+  const posts = [
+    {
+      id: 'dfadsfdsf23423423',
+      title: 'post 1',
+    },
+    {
+      id: 'dfadsfdsf234234233',
+      title: 'post 2',
+    },
+    {
+      id: 'dfadsfdsf234234234',
+      title: 'post 3',
+    }
+  ]
   return (
-    <div>
-      This is my Home
-    </div>
+    <Grid container>
+      {posts.map(post => (
+        <Card key={post.id}>
+          <CardContent>
+            <Typography>
+              {post.title}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </Grid>
   )
 }
 
